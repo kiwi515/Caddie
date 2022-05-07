74feda9ecc3ba2b1d8c5a730a3b5326b9eae44a0
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-05-07 15:51:10 -0400

    Remove old UI code, begin refactoring scene hook

--- include/RP/RPSystem/RPSysSceneCreator.h
+++ include/RP/RPSystem/RPSysSceneCreator.h
@@ -8,10 +8,46 @@
 class RPSysSceneCreator
 {
 public:
-    inline static RPSysSceneCreator * getInstance()
+    //! @brief RP Scene ID
+    //! @wfuname
+    enum ESceneID
     {
-        return sInstance;
-    }
+        SCENE_STRAP,
+        SCENE_SAVE_DATA_LOAD,
+        SCENE_TITLE,
+        SCENE_MII_SELECT,
+
+        SCENE_SWF,
+        SCENE_JSK,
+        SCENE_ARC,
+        SCENE_FLD,
+        SCENE_BSK,
+        SCENE_BWL,
+        SCENE_CAN,
+        SCENE_PNG,
+        SCENE_JSK,
+        SCENE_PLN,
+        SCENE_GLF,
+        SCENE_DGL,
+        SCENE_BIC,
+        SCENE_OMK,
+
+        SCENE_DEBUG_ROOT,
+        SCENE_SAVE_DATA_DEBUG,
+        SCENE_CONTEST_MII_DEBUG,
+        SCENE_MESSAGE_CHECK,
+        SCENE_LAYOUT_CHECK,
+        SCENE_LAYOUT_CHECK_2,
+        SCENE_NKJ_TEST_1,
+        SCENE_LAYOUT_TEST_1,
+        SCENE_SOUND_TEST_1,
+        SCENE_CPU_MII,
+        
+        SCENE_MAX
+    };
+
+public:    
+    static RPSysSceneCreator * getInstance() { return sInstance; }
 
     void changeSceneAfterFade(int id, nw4r::ut::Color *fadeColor);
 
--- include/caddie/core/caddieSceneHook.h
+++ include/caddie/core/caddieSceneHook.h
@@ -5,15 +5,47 @@
 
 namespace caddie
 {
-    class SceneHook
+    typedef void (* SceneCallback)(RPSysScene *);
+    typedef void (* PauseCallback)(RPSysScene *, bool isPause);
+
+    struct SceneHook
+    {
+        SceneCallback onConfigure;
+        SceneCallback onLoadResource;
+        SceneCallback onCalculate;
+        SceneCallback onUserDraw;
+        SceneCallback onExit;
+        PauseCallback onPause;
+    };
+
+    class SceneHookMgr
     {
     public:
-        static UNKTYPE * OnInit(UNKTYPE *);
-        static UNKTYPE * OnFrame(UNKTYPE *);
-        static bool OnExit(bool);
+        static SceneHookMgr& GetInstance()
+        {
+            static SceneHookMgr instance;
+            return instance;
+        }
+
+        void SetHook(RPSysSceneCreator::ESceneID scene, const SceneHook& hook)
+        {
+            mSceneHooks[scene] = hook;
+        }
+
+        static void ConfigureCallback();
+        static void LoadResourceCallback();
+        static void CalculateCallback();
+        static void UserDrawCallback();
+        static void ExitCallback();
+        static void PauseCallback();
+        static void UnPauseCallback();
 
     private:
+        SceneHookMgr() {}
+        virtual ~SceneHookMgr() {}
 
+    private:
+        SceneHook mSceneHooks[RPSysSceneCreator::SCENE_MAX];
     };
 }
 
